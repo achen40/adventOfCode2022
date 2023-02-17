dc79fa9e2d2e4c283dc5730e739ead4505e62110
Alberto Chen <achen40@stuy.edu>
2023-02-17 14:03:16 -0500

    stuck on day4 part 2

--- Day4.java
+++ Day4.java
@@ -3,7 +3,23 @@ import java.io.*;
 public class Day4{
   public static void main(String[] args) throws Exception{
       Scanner input = new Scanner(new File("Day4.txt"));
-      int count = 0;
+      // int count = 0;
+      // while(input.hasNextLine()){
+      //   String assignment = input.nextLine();
+      //   String[]pairs = assignment.split(",");
+      //   String[]elf1 = pairs[0].split("-");
+      //   String[]elf2 = pairs[1].split("-");
+      //   int elf1min = Integer.parseInt(elf1[0]);
+      //   int elf1max = Integer.parseInt(elf1[1]);
+      //   int elf2min = Integer.parseInt(elf2[0]);
+      //   int elf2max = Integer.parseInt(elf2[1]);
+      //   if((elf1min >= elf2min && elf1max <= elf2max) || (elf2min >= elf1min && elf2max <= elf1max)){
+      //     count++;
+      //   }
+      // }
+      // System.out.println("Count : "+count);
+
+      int overlapCount = 0;
       while(input.hasNextLine()){
         String assignment = input.nextLine();
         String[]pairs = assignment.split(",");
@@ -13,33 +29,14 @@ public class Day4{
         int elf1max = Integer.parseInt(elf1[1]);
         int elf2min = Integer.parseInt(elf2[0]);
         int elf2max = Integer.parseInt(elf2[1]);
-        if((elf1min >= elf2min && elf1max <= elf2max) || (elf2min >= elf1min && elf2max <= elf1max)){
-          count++;
-        }
-      }
-      System.out.println("Count : "+count);
-
-      File file = new File("Day4.txt");
-      Scanner input1 = new Scanner(new File("Day4.txt"));
-      int totalPairs = 0;
-      int overlapCount = 0;
-      while(input1.hasNextLine()){
-        totalPairs++;
-        String assignment = input1.nextLine();
-        String[]pairs = assignment.split(",");
-        String[]elf1 = pairs[0].split("-");
-        String[]elf2 = pairs[1].split("-");
-        int elf1min = Integer.parseInt(elf1[0]);
-        int elf1max = Integer.parseInt(elf1[1]);
-        int elf2min = Integer.parseInt(elf2[0]);
-        int elf2max = Integer.parseInt(elf2[1]);
-        if(elf1min >= elf2min && elf1max <= elf2max){
-          overlapCount++;
-        }
-        else if (elf2min >= elf1min && elf2max <= elf1max){
+        // System.out.println("elf1min: "+elf1min);
+        // System.out.println("elf1max: "+elf1max);
+        // System.out.println("elf2min: "+elf2min);
+        // System.out.println("elf2max: "+elf2max);
+        if((elf1min <= elf2min && elf2min <= elf1max) || (elf1min <= elf2max && elf2max <= elf1max)){
           overlapCount++;
         }
       }
-      System.out.println("Count : "+(totalPairs - count));
+      System.out.println("Count : "+overlapCount);
   }
 }
